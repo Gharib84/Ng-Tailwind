cc8389e67534aa20a50ca4560fbace1e1b23f5ab
gharib84 <itprofessional63@gmail.com>
2023-03-17 18:23:48 +0100

    Ng-tailwind Package Has Been Finished Except spinner

--- install.js
+++ install.js
@@ -6,27 +6,36 @@ const { execSync } = require('child_process');
 inquirer
     .prompt([
         {
+            type: 'input',
+            name: 'projectName',
+            message: 'What Is The name Of Project That You Would Like',
+
+        },
+
+        {
             type: 'list',
             name: 'designLibrary',
             message: 'Which Design Library Would You Like To Use ? ',
             choices: ['TailwindCss', 'Material Design bootstrap']
 
         }
-    ]).then((userAnswer) => {
+    ],).then((userAnswer) => {
         try {
             execSync('ng version')
         } catch (error) {
             execSync('npm install -g @angular/cli');
+           
         }
         // Create new Angular project
-        execSync('ng new my-app --style=scss --routing');
+        execSync(`ng new ${userAnswer.projectName} --style=scss --routing`);
         if (userAnswer.designLibrary === 'TailwindCss') {
             execSync('npm install -D tailwindcss postcss autoprefixer');
             execSync('npx tailwindcss init');
+            
 
-            const stylesFilePath = path.join(process.cwd(), 'my-app', 'src', 'styles.scss');
+            const stylesFilePath = path.join(process.cwd(), `${userAnswer.projectName}`, 'src', 'styles.scss');
             fs.appendFileSync(stylesFilePath, "@import 'tailwindcss/base';\n@import 'tailwindcss/components';\n@import 'tailwindcss/utilities';\n");
-            const configFilePath = path.join(process.cwd(), 'my-app', 'tailwind.config.js');
+            const configFilePath = path.join(process.cwd(), `${userAnswer.projectName}`, 'tailwind.config.js');
             const config = "module.exports = {\n  content: [\n    './src/**/*.{html,ts}',\n  ],\n  theme: {\n    extend: {},\n  },\n  plugins: [],\n};\n";
             fs.writeFileSync(configFilePath, config);
         } else {
